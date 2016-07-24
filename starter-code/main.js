7e60ad30b35a62ddba31749f060c954022eeee4f
Travis Hayes <t.hayes@sample.com>
2016-07-24 11:48:43 -0700

    updated html and logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,19 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo){
+	alert("That's A Match!");
+} else if (cardOne === cardThree) {
+	alert("That's Not A Match");
+} else if (cardOne === cardFour) {
+	alert("That's Not A Match.");
+} else if (cardTwo === cardThree) {
+	alert("That's Not A Match.");
+} else if (cardTwo === cardFour) {
+	alert("That's Not A Match.");
+} else if (cardThree === cardFour) {
+	alert("That's A Match!");
+} else {
+	alert("You Made A Invalid Selection");
+}
