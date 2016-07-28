e609b436861ba6a03688aad7df75b5ca8cf907da
Travis Hayes <t.hayes@sample.com>
2016-07-27 19:44:55 -0700

    updated logic and style

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,20 +1,40 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-if (cardOne === cardTwo){
-	alert("That's A Match!");
-} else if (cardOne === cardThree) {
-	alert("That's Not A Match");
-} else if (cardOne === cardFour) {
-	alert("That's Not A Match.");
-} else if (cardTwo === cardThree) {
-	alert("That's Not A Match.");
-} else if (cardTwo === cardFour) {
-	alert("That's Not A Match.");
-} else if (cardThree === cardFour) {
-	alert("That's A Match!");
-} else {
-	alert("You Made A Invalid Selection");
+var board = document.getElementById('game-board');
+
+function createBoard(){
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+
+		board.appendChild(cardElement);
+		board.appendChild(cardElement);
+	}
 }
+
+function isTwoCards(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	//grab ther data-type attribute onclick 
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king'){
+		this.innerHTML = "<img src='http://i.imgur.com/OwuzXcb.jpg'>";
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/t1tHP7p.jpg'>";
+	}
+	if(cardsInPlay.length === 2){
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+function isMatch(cards){
+	if (cards[0] === cards[1]) {
+		alert("You Found A Match");
+	} else {
+		alert("Not A Match, try Again");
+	}
+}
+createBoard();
